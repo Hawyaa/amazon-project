dafc83e18d345034bb491908cec49a80a0cb6ef7
Hawi <hawi4936@gmail.com>
2025-09-28 21:22:27 -0700

    fix all import case sensitivity

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useContext } from 'react';
 import Routing from './Router.jsx';
-// import { DataProvider, DataContext } from '../components/DataProvider/DataProvider.jsx';
+import { DataProvider, DataContext } from './components/DataProvider/DataProvider.jsx';
 import { reducer, initialState } from './Utility/reducer';
 import { Type } from './Utility/action.type';
 import { auth } from './Utility/firebase';
--- src/Pages/Landing/Landing.jsx
+++ src/Pages/Landing/Landing.jsx
@@ -1,17 +1,17 @@
-import React from "react";
-// import Layout from "../../components/Layout/Layout";
-import Carousel from "../../components/Carousel/CarouselEffect";
-import Category from "../../components/Category/Category";
-import Product from "../../components/Product/Product";
+import React from 'react'
+import Layout from '../../components/Layout/Layout'
+import Carousel from '../../components/Carousel/CarouselEffect'
+import Category from '../../components/Category/Category'
+import Product from '../../components/Product/Product'
 
 function Landing() {
-  return (
-    <Layout>
-      <Carousel />
-      <Category />
-      <Product />
-    </Layout>
-  );
+    return (
+        <Layout>
+            <Carousel/>
+            <Category/>
+            <Product/>
+        </Layout>
+    )
 }
 
-export default Landing;
+export default Landing
